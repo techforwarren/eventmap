2ef32aa0dd027ac287c4c1d2920601131e2f9743
Mick Thompson <dthompson@gmail.com>
2019-09-28 18:55:25 -0700

    gzip json events file

--- fetch-events.sh
+++ fetch-events.sh
@@ -6,5 +6,7 @@ python3 events/fetch.py all-events.json
 
 python3 events/filter.py all-events.json events.json
 
-aws s3 cp events.json $S3_PATH/events.json
+gzip events.json
+
+aws s3 cp events.json.gz $S3_PATH/events.json --acl public-read --metadata "Content-Encoding=gzip,Content-Type=application/json"
 
