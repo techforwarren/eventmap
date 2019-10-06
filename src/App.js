994e65d07b502732ffca55d7b1ec9259837e1537
jasonkalmeida <jasonkalmeida@gmail.com>
2019-10-06 09:45:11 -0700

    Working functionality

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React, {useState, useEffect } from 'react';
+import {isMobile} from 'react-device-detect';
 import SearchBar from './SearchBar';
 import Map from './Map';
+import MobileList from './MobileList';
 import './App.scss';
 
 function App() {
@@ -12,6 +14,8 @@ function App() {
   const [hoverEvent, setHoverEvent] = useState(null);
   //Current selected location location filter
   const [locFilt, setLocFilt] = useState(null);
+  //For mobile, the current card
+  const [cardIndex, setCardIndex] = useState(null);
 
   //Makes API call when zipcode entered
   useEffect(() => {
@@ -23,13 +27,28 @@ function App() {
       //Reset states on new zipcode
       setHoverEvent(null);
       setLocFilt(null);
+      setCardIndex(0);
 
     }
   }, [currZip]);
 
+  useEffect(() => {
+    console.log("CARDINDEX UPDATED");
+    if(isMobile && events != null){
+      setHoverEvent((('location' in events[cardIndex] && 'location' in events[cardIndex]['location'] && 'latitude' in events[cardIndex]['location']['location']) ? "" + events[cardIndex]['location']['location']['latitude'] + "&" + events[cardIndex]['location']['location']['longitude'] : null));
+
+
+    }
+
+  }, [cardIndex]);
+
+
   return (
     <div className="app">
       <SearchBar currZip={currZip} updateZip={(newZip) => setCurrZip(newZip)} events={events} updatedHover={(newHover) => setHoverEvent(newHover)} locFilt={locFilt}/>
+      {events !== null && isMobile &&
+        <MobileList events={events} updatedHover={(newHover) => setHoverEvent(newHover)} locFilt={locFilt} cardIndex={cardIndex} updateCardIndex={(update) => setCardIndex(update)}/>
+      }
       <Map currZip={currZip} events={events} hoverMarker={hoverEvent} selectLoc={(newLoc) => setLocFilt(newLoc)} locFilt={locFilt}/>
     </div>
   );
---
+++ src/MobileList.js
@@ -0,0 +1,124 @@
+import React, { useEffect } from 'react';
+import moment from 'moment';
+import groupBy from 'lodash.groupby';
+import sortBy from 'lodash.sortby';
+require('twix');
+
+const MAX_DAYS_IN_LIST = 1;
+
+function EventTimes(props) {
+  const { rawTimes } = props;
+  let sortedTimesByDate = groupBy(sortBy(rawTimes,
+    // Sort all of the ranges by when they start;
+    // Unix returns the millisecond time, so all
+    // events will be different
+    (item) => { return item.start.unix() }),
+    // Group the ranges by the day they happen on;
+    // fully including the year, month, and day
+    // in that order guarantees that normal sorting
+    // will respect 9/31 v 10/1, and 2020/01 vs 2019/12
+    (item) => { return item.start.format('YYYY-MM-DD') }
+  )
+
+  let sortedDates = Object.keys(sortedTimesByDate).sort();
+
+  const dateRowFactory = (date) => {
+    let times = sortedTimesByDate[date];
+    let dayStr = times[0].start.format('ddd M/D')
+    let timeStrs = times.map((time) => time.range.format({ hideDate : true })).join(', ')
+    return (
+      <p key={`time-${date}`}>
+        { dayStr }{' | '}{ timeStrs }
+      </p>
+    )
+  }
+
+  if (sortedDates.length <= MAX_DAYS_IN_LIST) {
+    return sortedDates.map(dateRowFactory)
+  } else {
+    let nextDay = sortedDates[MAX_DAYS_IN_LIST - 1];
+    let lastDay = sortedDates[sortedDates.length - 1];
+    let nextStart = sortedTimesByDate[nextDay][0].start;
+    let lastStart = sortedTimesByDate[lastDay][0].start;
+    return sortedDates.slice(0,MAX_DAYS_IN_LIST - 1).map(dateRowFactory).concat(
+      <p>
+        More Times from {nextStart.twix(lastStart, { allDay : true }).format()}
+      </p>
+    )
+  }
+}
+
+export function MobileList(props){
+
+  useEffect(() => {
+
+    //Location filter
+    if(props.locFilt != null){
+
+      for(let x = 0; x < props.events.length; x++) {
+        let event = props.events[x];
+        if(!('location' in props.events[props.cardIndex]) || !('location' in event['location']) || props.events[props.cardIndex]['location']['location']['latitude'] !== props.locFilt['lat'] || props.events[props.cardIndex]['location']['location']['longitude'] !== props.locFilt['lng']){
+          if('location' in event && 'location' in event['location'] && 'latitude' in event['location']['location']){
+            if(event['location']['location']['latitude'] === props.locFilt['lat'] && event['location']['location']['longitude'] === props.locFilt['lng']){
+              console.log(x);
+              console.log(props.cardIndex);
+              props.updateCardIndex(x);
+              x = props.events.length;
+            }
+          }
+
+
+        }
+      }
+
+    }
+
+  }, [props.locFilt])
+
+
+
+  const listEvents = props.events.map((event, index) => {
+
+    // Normalize Mobilize's time formatting into
+    // easy-to-use moments
+    let rawTimes = event['timeslots'].map((timeslot) => {
+      let start = moment(timeslot.start_date * 1000);
+      let end = moment(timeslot.end_date * 1000);
+      return {
+        start, end,
+        range: start.twix(end)
+      }
+    })
+
+    return (
+      <a href={event['browser_url']}
+        className="eventCard"
+        target="_blank"
+        key={event['id']}
+        coord={('location' in event && 'location' in event['location'] && 'latitude' in event['location']['location']) ? "" + event['location']['location']['latitude'] + "&" + event['location']['location']['longitude'] : ""}
+        onMouseEnter={(event) => { props.updatedHover(event['currentTarget'].getAttribute('coord')) }}
+        onMouseLeave={(event) => { props.updatedHover(null) }}>
+        <div className="mobileInfo">
+          <h3>{event['title']}</h3>
+          <p><strong>{event['location']['venue']}</strong> in <strong>{event['location']['locality']}</strong></p>
+          <EventTimes rawTimes={rawTimes} />
+          <p className="eventRSVP">Click to RSVP</p>
+        </div>
+
+      </a>
+
+    )
+  }).filter((arrItem) => {
+    return arrItem != null;
+  });
+
+  return (
+    <div className="mobileList">
+      {listEvents[props.cardIndex]}
+      {console.log(listEvents)}
+      <button onClick={() => props.updateCardIndex(props.cardIndex+1)}>Click me</button>
+    </div>
+  );
+}
+
+export default MobileList;
