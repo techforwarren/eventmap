26eab0abd46d2f5cbbf6ebc170dc32af79b472b3
Erik Ostrom <erik@echographia.com>
2019-10-21 12:16:00 -0500

    Added (empty) alt text to landing page icon.
    The icon after "SHE HAS A PLAN FOR THAT" doesn't have any semantic
    content, it's just decorative. I added an empty alt text so that
    accessibility checkers will know it's empty on purpose.

--- src/App.js
+++ src/App.js
@@ -68,7 +68,7 @@ function App() {
       <SearchBar currZip={currZip} updateZip={(newZip) => setCurrZip(newZip)} events={events} updatedHover={(newHover) => setHoverEvent(newHover)} locFilt={locFilt}/>
       {events === null && currZip == null &&
         <div id="startLoad">
-          <h1 id="firstLine">SHE HAS</h1><h1 id="secondLine">EVENTS</h1><h1 id="thirdLine">FOR THAT <img src={gMark}></img></h1>
+          <h1 id="firstLine">SHE HAS</h1><h1 id="secondLine">EVENTS</h1><h1 id="thirdLine">FOR THAT <img src={gMark} alt=""></img></h1>
           <h3 id="searchCTA">Enter your zipcode to find events near you!</h3>
         </div>
       }
