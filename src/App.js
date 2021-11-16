f198f2510154141a62ef678542928cda2f060f4d
Shai Sachs <shaisachs@gmail.com>
2021-11-15 21:02:41 -0500

    parameterize orgid
    Signed-off-by: Shai Sachs <shaisachs@gmail.com>

--- src/App.js
+++ src/App.js
@@ -12,6 +12,7 @@ import ReactGA from 'react-ga';
 ReactGA.initialize('UA-149839620-1');
 ReactGA.pageview(window.location.pathname + window.location.search);
 
+const mobilizeOrgId = 1316;
 const queryString = require('query-string');
 
 /*
@@ -156,7 +157,7 @@ function App() {
   //Makes API call when zipcode entered or the range is updated
   useEffect(() => {
     if(currZip != null){
-      fetch("https://api.mobilize.us/v1/organizations/1316/events?timeslot_start=gte_now&per_page=200&zipcode=" + currZip + "&max_dist=" + currRange)
+      fetch("https://api.mobilize.us/v1/organizations/" + mobilizeOrgId + "/events?timeslot_start=gte_now&per_page=200&zipcode=" + currZip + "&max_dist=" + currRange)
       .then((res)=>res.json())
       .then((data)=>setEvents(data['data']));
 
